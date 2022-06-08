9f5261db9ab1d6064cc5aec1ec106e5ce1fcd17c
ibmpariwesh <pariweshg@gmail.com>
2022-06-08 05:38:25 +0000

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from './app.component';
 import { MymoduleModule } from './mymodule/mymodule.module';
+import { AboutComponent } from './about/about.component';
+import { RouterModule, Routes } from "@angular/router";
 
+const routes: Routes=[
+  {path:'about', component:AboutComponent}
+]
 @NgModule({//decorator
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent
   ],
-  imports: [MymoduleModule,FormsModule, HttpClientModule,
+  imports: [MymoduleModule,FormsModule, HttpClientModule,RouterModule.forRoot(routes),
     BrowserModule
   ],
   providers: [],
